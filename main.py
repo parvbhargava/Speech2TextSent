a9c1fb622ec50638845adf6da9baaee7ddfa6bda
parvbhargava <58410910+parvbhargava@users.noreply.github.com>
2022-05-23 21:35:25 +0530

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+from s2t import get_large_audio_transcription
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+path = "include/male.wav"
+get_large_audio_transcription(path)
+
+# Sentiment Analysis
+Sentence=[str(text)]
+analyser=SentimentIntensityAnalyzer()
+for i in Sentence:
+    v=analyser.polarity_scores(i)
+    print(v)
